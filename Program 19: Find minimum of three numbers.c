b2109ef03d069458483366d822ad93a92f34cdee
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-02 20:52:10 +0530

    Create Program 19: Find minimum of three numbers.c

---
+++ Program 19: Find minimum of three numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a <= b && a <= c) {
+        printf("The smallest number is %d\n", a);
+    } else if (b <= a && b <= c) {
+        printf("The smallest number is %d\n", b);
+    } else {
+        printf("The smallest number is %d\n", c);
+    }
+}
