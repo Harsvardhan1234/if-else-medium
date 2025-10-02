42c50f4c8a6c30d1828850302c7042e00ce61605
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-02 20:51:18 +0530

    Create Program 18: Find maximum of three numbers.c

---
+++ Program 18: Find maximum of three numbers.c
@@ -0,0 +1,15 @@
+    # include <stdio.h>
+    void main(){
+    int a, b, c;
+    printf("enter three side of the no.:");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a>=b && a>=c)
+     {
+        printf("The largest number is %d\n",a);
+    }else if(b>=a && b>=c){
+    printf("The largest number is %d\n",b);
+    }else{
+         printf("The largest number is %d\n",c);
+    }
+    }
