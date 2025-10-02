c8a261577aee7ce34eff176e789a23be13f5d2aa
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-02 20:39:52 +0530

    Create Program 17: Check if a triangle is scalene.c

---
+++ Program 17: Check if a triangle is scalene.c
@@ -0,0 +1,11 @@
+    # include <stdio.h>
+    void main(){
+    int a, b, c;
+    printf("enter three side of the triangle:");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a!=b && b!=c && c!=a){
+        printf("the triangle is scalen\n");
+    }else{
+    printf("the triangle is not scalen\n");
+    }
+    }
