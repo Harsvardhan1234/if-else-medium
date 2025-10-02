fa9626a3504528c021e196fd1db8f98d13fb0a4d
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-02 21:06:33 +0530

    Create Program 23: Check if a number is a three-digit  even number.c

---
+++ Program 23: Check if a number is a three-digit  even number.c
@@ -0,0 +1,13 @@
+    # include <stdio.h>
+    void main(){
+    int a;
+    printf("enter the number:");
+    scanf("%d",&a);
+
+    if(a >=100 && a <=999 && a % 2 ==0)
+     {
+        printf("The number is a three-digit even number. %d\n",a);
+    }else{
+         printf("The number is not a three-digit even number. %d\n",a);
+    }
+    }
